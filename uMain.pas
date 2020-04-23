ab5b766a73a32007811111a8768ee40455bee1da
André Luis <dolby1games@gmail.com>
2020-04-23 02:29:19 -0300

    Added initial codes

---
+++ uMain.pas
@@ -0,0 +1,48 @@
+unit uMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Buttons, dxGDIPlusClasses,
+  Vcl.ExtCtrls, cxGraphics, cxLookAndFeels, cxLookAndFeelPainters, Vcl.Menus,
+  Vcl.StdCtrls, VCLTee.TeCanvas, cxButtons;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses
+  ShellApi;
+
+{$R *.dfm}
+
+
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  Dir: String;
+begin
+  Dir := ExtractFilePath(Application.ExeName);
+
+  ShellExecute(Handle, nil, PChar(Dir + 'src\suspend.exe'), 'gta5.exe', PChar(Dir + 'src\'), SW_SHOWNORMAL);
+  Sleep(6000);
+  ShellExecute(Handle, nil, PChar(Dir + 'src\suspend.exe'), '-r gta5.exe', PChar(Dir + 'src\'), SW_SHOWNORMAL);
+  Sleep(1000);
+  Application.Terminate;
+end;
+
+end.
